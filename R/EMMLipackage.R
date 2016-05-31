e59a7f21e6d8e92f693fcb582e03a942a2eeb83e
timcdlucas <timcdlucas@gmail.com>
2016-05-31 16:07:26 +0100

    Fix url in docs and rebuild.

--- R/EMMLipackage.R
+++ R/EMMLipackage.R
@@ -8,7 +8,7 @@
 #'
 #'  The publication describing this analysis is A. Goswami1 and J. Finarelli
 #'    (2016) EMMLi: A maximum likelihood approach to the analysis of modularity.
-#'    Evolution \link{http://onlinelibrary.wiley.com/doi/10.1111/evo.12956/abstract}.
+#'    Evolution \url{http://onlinelibrary.wiley.com/doi/10.1111/evo.12956/abstract}.
 #'
 #'@name EMMLipackage
 #'@docType package
