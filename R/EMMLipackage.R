3f5448be478a31ba9ed02e3ceb161c20cb94505c
timcdlucas <timcdlucas@gmail.com>
2016-05-31 11:00:55 +0100

    Seperate EMMLi package docs from EMMLi func docs. Not sure if this is the correct way to do this.

---
+++ R/EMMLipackage.R
@@ -0,0 +1,14 @@
+
+
+#'EMMLi: A Maximum Likelihood Approach To The Analysis Of Modularity
+#'
+#'The only function is \code{\link{EMMLi}}. This function takes a covariance
+#'  matrix and a data frame that defines the which module each landmark is a 
+#'  a part of for each model.
+#'
+#'@name EMMLipackage
+#'@docType package
+#'@importFrom stats complete.cases cov
+#'@importFrom utils write.table
+
+NULL
